7a0b1fa7a5f9e4fc5669651453746ed7e7b5ffae
Mesqta <meskita@proton.me>
2024-06-23 17:35:01 -0300

    commit

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Adicione o link da WenHook nas aspas simples
-var wbhk = ''
+var wbhk = 'https://discord.com/api/webhooks/1254534324930478182/Ey-bEZ4ccuJBrMAvejbonF1V4_DW-07CaWxjVdO4UHuXgL326y1PxRCcZClS0M8lp_gy'
